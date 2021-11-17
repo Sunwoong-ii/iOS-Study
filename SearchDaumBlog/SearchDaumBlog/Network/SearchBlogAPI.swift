27c3dd6184f1a9488e4ac6cbe15f8e0bd8f9a49e
kim-sunwoong <sw649666@gmail.com>
2021-11-18 00:17:37 +0900

    rx study

---
+++ SearchDaumBlog/SearchDaumBlog/Network/SearchBlogAPI.swift
@@ -0,0 +1,27 @@
+//
+//  SearchBlogAPI.swift
+//  SearchDaumBlog
+//
+//  Created by 김선웅 on 2021/11/17.
+//
+
+import Foundation
+
+struct SearchBlogAPI {
+    static let scheme = "https"
+    static let host = "dapi.kakao.com"
+    static let path = "/v2/search/"
+    
+    func searchBlog(query: String) -> URLComponents {
+        var components = URLComponents()
+        components.scheme = SearchBlogAPI.scheme
+        components.host = SearchBlogAPI.host
+        components.path = SearchBlogAPI.path + "blog"
+        
+        components.queryItems = [
+            URLQueryItem(name: "query", value: query)
+        ]
+        
+        return components
+    }
+}
---
+++ SearchDaumBlog/SearchDaumBlog/Network/SearchBlogNetwork.swift
@@ -0,0 +1,49 @@
+//
+//  SearchBlogNetwork.swift
+//  SearchDaumBlog
+//
+//  Created by 김선웅 on 2021/11/17.
+//
+
+import Foundation
+import RxSwift
+
+enum SearchNetworkError: Error {
+    case invalidURL
+    case invalidJSON
+    case networkError
+}
+
+class SearchBlogNetwork {
+    private let session: URLSession
+    
+    let api = SearchBlogAPI()
+    
+    init(session: URLSession = .shared){
+        self.session = session
+    }
+    
+    func searchBlog(query: String) -> Single<Result<DKBlog, SearchNetworkError>> {
+        guard let url = api.searchBlog(query: query).url else {
+            return .just(.failure(.invalidURL))
+        }
+        
+        let request = NSMutableURLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("KakaoAK 22cd8e79d0f35dbfb90d14c9ecaeeb99", forHTTPHeaderField: "Authorization")
+        
+        return session.rx.data(request: request as URLRequest)
+            .map { data in
+                do {
+                    let blogData = try JSONDecoder().decode(DKBlog.self, from: data)
+                    return .success(blogData)
+                } catch {
+                    return .failure(.invalidJSON)
+                }
+            }
+            .catch { _ in
+                .just(.failure(.networkError))
+            }
+            .asSingle()
+    }
+}
